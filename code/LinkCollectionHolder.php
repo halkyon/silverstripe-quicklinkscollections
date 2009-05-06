e85708f9747d98ab5f6eba918af215e50abd7f27
sharvey <sharvey@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-05-06 22:34:24 +0000

    MINOR Hide tabs not required
    git-svn-id: http://svn.silverstripe.com/open/modules/quicklinkscollections/trunk@76285 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/LinkCollectionHolder.php
+++ code/LinkCollectionHolder.php
@@ -8,6 +8,18 @@ class LinkCollectionHolder extends Page {
 	
 	public static $default_child = 'LinkCollection';
 	
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+		
+		$fields->removeByName('Metadata');
+		$fields->removeByName('Sidebar');
+		$fields->removeByName('Widgets');
+		$fields->removeByName('To-do');
+		$fields->removeByName('Reports');
+		
+		return $fields;
+	}
+	
 }
 class LinkCollectionHolder_Controller extends Page_Controller {
 	
